9b9bf1c32797e6235b4e58a10e14827ad01d35df
TrungHauDinh <trunghaukmhd@gmail.com>
2020-07-31 17:02:28 +0700

    Add First Custom View

---
+++ app/src/main/java/vn/trunghaudinh/canvas/activity/Example1Activity.kt
@@ -0,0 +1,12 @@
+package vn.trunghaudinh.canvas.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import vn.trunghaudinh.canvas.R
+
+class Example1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_example1)
+    }
+}
---
+++ app/src/main/java/vn/trunghaudinh/canvas/custom_view/CustomView.kt
@@ -0,0 +1,8 @@
+package vn.trunghaudinh.canvas.custom_view
+
+import android.content.Context
+import android.view.View
+
+class CustomView(context : Context) : View(context) {
+
+}
